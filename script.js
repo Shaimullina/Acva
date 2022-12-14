97445224b4df20aa9260984a741af09924d4f996
Nadejda <zagzina2017@yandex.ru>
2022-12-14 07:16:30 +0300

    Menu

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let menuBtn = document.querySelector('.hamburger');
+let menuList  = document.querySelector('.menu');
 
 menuBtn.addEventListener('click', function() {
     menuBtn.classList.toggle('open');
+    menuList.classList.toggle('open');
 });
-
-
